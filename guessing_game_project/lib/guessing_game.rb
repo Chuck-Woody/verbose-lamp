a0826fc51c0af1491141905bf325ec4b6fe1150d
chuck <you@example.com>
2022-03-01 09:31:48 -0800

    W2D2

---
+++ guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,23 @@
+class GuessingGame
+
+    def initialize(min,max)
+        @min = min
+        @max = max
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+
+    def num_attempts
+        @num_attempts
+    end
+
+    def game_over?
+        @game_over
+    end
+    def check_num(num)
+        @game_over = true if num == @secret_num
+        @num_attempts += 1
+    end
+end
