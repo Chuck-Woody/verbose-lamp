7f1b5cc25e18ed81d1537c7eb4863099bbe388bd
chuck <you@example.com>
2022-03-01 14:20:26 -0800

    has vacancy refactor

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,51 @@
 require_relative "room"
 
 class Hotel
+    attr_reader :rooms
+    def initialize(name, hash)
+        @rooms = {}
+        @name = name
+        hash.each do |k,v|
+
+            @rooms[k] = Room.new(v)
+        end
   
+    end
+
+        def name
+            @name.split.map {|word| word.capitalize}.join(" ")
+        end
+
+        def room_exists?(name)
+
+            @rooms[name] != nil
+        end
+
+        def check_in(person,room)
+
+            if room_exists?(room)
+                
+                if @rooms[room].add_occupant(person)
+                    print 'check in successful'
+                else
+                    print 'sorry, room is full'
+                end
+
+              
+
+            else
+                print 'sorry, room does not exist' 
+            end
+        end
+
+        def has_vacancy?
+            
+            # @rooms.each {| _ ,v|  return true if !v.full? }
+            @rooms.values.any? {|v| !v.full?}
+            # false
+        end
+
+        def list_rooms
+
+        end
 end
--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,30 @@
 class Room
-  
+
+    attr_reader :capacity, :occupants
+
+  def initialize(num)
+    @capacity = num
+    @occupants = []
+    
+  end
+
+  def full?
+
+    @occupants.length >= @capacity
+  end
+
+  def available_space
+
+    capacity - @occupants.length
+  end
+
+  def add_occupant(str)
+
+    if full?
+        return false
+    else
+        @occupants << str
+        return true
+    end
+  end
 end
