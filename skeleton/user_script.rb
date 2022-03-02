3ae487c592083276e66638e825cf8b81c0c44d69
chuck <you@example.com>
2022-03-02 09:12:06 -0800

    30% done with Best Friend Error Handling

---
+++ skeleton/user_script.rb
@@ -0,0 +1,14 @@
+require_relative 'super_useful'
+
+# puts "'five' == #{convert_to_int('five')}"
+begin
+    feed_me_a_fruit
+rescue
+     
+     
+end
+sam = BestFriend.new('Sam', 1, 'Soccer')
+
+sam.talk_about_friendship
+sam.do_friendstuff
+sam.give_friendship_bracelet
